4e575fa05d5219ca363a930f07003d6a8af46168
Daniel Buxton Sierras <alu.129152@usj.es>
2025-02-18 16:14:10 +0100

    Modify packet handling logic

--- Telemetry.py
+++ Telemetry.py
@@ -1,23 +1,25 @@
 from ttkbootstrap import Window, Notebook, Frame, Canvas, Menu, Label
 import sys
 
-screen = Window(themename="darkly")
+main_application_window = Window(themename="darkly")
 
 import json
 import time
 from utils.dictionnaries import *
-import packet_parser.packet_management as pm
-import packet_parser.parser2024 as parser
-from models.Custom_Frame import DriversFrame, DataReceptionFrame, WeatherForecastFrame
+import packet_handler.PacketManager as pm
+import packet_handler.PackerParser as parser
+from models.frames.DriversFrame import DriversFrame
+from models.frames.DataReceptionFrame import DataReceptionFrame
+from models.frames.WeatherForecastFrame import WeatherForecastFrame
 
 
 def init_window():
     global map_canvas
-    screen.columnconfigure(0, weight=1)
-    screen.rowconfigure(0, pad=75)
-    screen.rowconfigure(1, weight=1)
+    main_application_window.columnconfigure(0, weight=1)
+    main_application_window.rowconfigure(0, pad=75)
+    main_application_window.rowconfigure(1, weight=1)
 
-    screen.title("Telemetry Application")
+    main_application_window.title("Telemetry Application")
 
     top_frame.grid(row=0, column=0, columnspan=3, sticky="nsew")
     main_frame.grid(row=1, column=0, sticky="nsew")
@@ -50,15 +52,15 @@ def init_window():
     top_label2.place(relx=1, rely=0.5, anchor='e', relheight=1)
     top_frame.columnconfigure(0, weight=3)
 
-    screen.geometry("1480x800")
-    screen.protocol("WM_DELETE_WINDOW", close_window)
+    main_application_window.geometry("1480x800")
+    main_application_window.protocol("WM_DELETE_WINDOW", close_window)
 
-    menubar = Menu(screen)
+    menubar = Menu(main_application_window)
     filemenu = Menu(menubar, tearoff=0)
     filemenu.add_command(label="PORT Selection", command=lambda : pm.port_selection(dictionnary_settings, listener, PORT))
     filemenu.add_command(label="UDP Redirect", command=lambda : pm.UDP_Redirect(dictionnary_settings, listener, PORT))
     menubar.add_cascade(label="Settings", menu=filemenu)
-    screen.config(menu=menubar)
+    main_application_window.config(menu=menubar)
 
 
 def close_window():
@@ -79,8 +81,8 @@ with open("settings.txt", "r") as f:
 if len(sys.argv)==2:
     dictionnary_settings["port"] = int(sys.argv[1])
 
-top_frame = Frame(screen)
-main_frame = Frame(screen)
+top_frame = Frame(main_application_window)
+main_frame = Frame(main_application_window)
 
 top_label1 = Label(top_frame, text="Course ", font=("Arial", 24))
 top_label2 = Label(top_frame, text="", font=("Arial", 24), width=10)
@@ -97,7 +99,7 @@ listener = parser.Listener(port=PORT[0],
 
 function_hashmap = { #PacketId : (fonction, arguments)
     0: (pm.update_motion, (map_canvas, None)),
-    1: (pm.update_session, (top_label1, top_label2, screen, map_canvas)),
+    1: (pm.update_session, (top_label1, top_label2, main_application_window, map_canvas)),
     2: (pm.update_lap_data, ()),
     3: (pm.warnings, ()),
     4: (pm.update_participants, ()),
@@ -126,8 +128,8 @@ while running:
         pm.frames[7].update(packet_received) #Packet Received tab
         pm.session.packet_received = packet_received[:]
         packet_received = [0]*15
-    screen.update()
-    screen.update_idletasks()
+    main_application_window.update()
+    main_application_window.update_idletasks()
     
 
 listener.socket.close()
